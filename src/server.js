e4f9e9552de35a0cc7cad35fe8631fcd47a37cc8
Aldahir Emmanuel Ballina Nuñez <aldahirballina@MacBook-Air-de-Aldahir.local>
2026-02-02 08:45:31 -0600

    feb 2

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const { queryP } = require('./dataBase/dbConnection');
 const { initCronJobs } = require('./services/birthday.service');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, '0.0.0.0', async () => {
   initCronJobs();
